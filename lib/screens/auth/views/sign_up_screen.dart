bd269f6df08ef9d087df373ec0d9d07170219667
appseedsorg <appseedsorg@gmail.com>
2024-02-19 16:19:43 +0530

    Auth_logic

--- lib/screens/auth/views/sign_up_screen.dart
+++ lib/screens/auth/views/sign_up_screen.dart
@@ -17,7 +17,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
   final passwordController = TextEditingController();
   final emailController = TextEditingController();
   final nameController = TextEditingController();
-  final ageController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
   IconData iconPassword = CupertinoIcons.eye_fill;
   bool obscurePassword = true;
@@ -37,7 +36,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
           setState(() {
             signUpRequired = false;
           });
-          // Navigator.pop(context);
         } else if (state is SignUpProcess) {
           setState(() {
             signUpRequired = true;
@@ -170,24 +168,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       return null;
                     }),
               ),
-              const SizedBox(height: 10),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.9,
-                child: MyTextField(
-                    controller: ageController,
-                    hintText: 'Age',
-                    obscureText: false,
-                    keyboardType: TextInputType.name,
-                    prefixIcon: const Icon(CupertinoIcons.checkmark_seal_fill),
-                    validator: (val) {
-                      if (val!.isEmpty) {
-                        return 'Please fill in this field';
-                      } else if (val.length > 30) {
-                        return 'Age too long';
-                      }
-                      return null;
-                    }),
-              ),
               SizedBox(height: MediaQuery.of(context).size.height * 0.02),
               !signUpRequired
                   ? SizedBox(
@@ -196,11 +176,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           onPressed: () {
                             if (_formKey.currentState!.validate()) {
                               MyUser myUser = MyUser.empty;
-                              // myUser = myUser.copyWith(
-                              // 	email: emailController.text,
-                              // 	name: nameController.text,
-                              //   age: int.parse(ageController.text),
-                              // );
+                              myUser.email = emailController.text;
+                              myUser.name = nameController.text;
                               setState(() {
                                 context.read<SignUpBloc>().add(SignUpRequired(
                                     myUser, passwordController.text));
