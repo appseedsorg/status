a9203a32621a81fa945a88f30e2064aa8ce90d77
appseedsorg <appseedsorg@gmail.com>
2024-02-28 20:18:30 +0530

    change-home-screen

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:status_app/screens/auth/blocs/sign_in_block/sign_in_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -10,26 +7,14 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.background,
-        title: Row(
-          children: [
-            Image.asset('assets/images/8.png', scale: 14),
-            const SizedBox(width: 8),
-            const Text(
-              'STATUS',
-              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30),
-            )
-          ],
+      body: const Center(
+        child: Text(
+          "Success",
+          style: TextStyle(
+            color: Colors.green,
+            fontSize: 24.0, // Adjust font size as needed
+          ),
         ),
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.cart)),
-          IconButton(
-              onPressed: () {
-                context.read<SignInBloc>().add(SignOutRequired());
-              },
-              icon: const Icon(CupertinoIcons.arrow_right_to_line)),
-        ],
       ),
     );
   }
