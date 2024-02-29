d15e1aa28594bcf8c2e23e4b407349f49c1070b5
appseedsorg <appseedsorg@gmail.com>
2024-02-29 09:55:25 +0530

    sign-in-page-created

--- lib/screens/auth/views/Login/login_screen.dart
+++ lib/screens/auth/views/Login/login_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'components/login_form.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:status_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:status_app/screens/auth/blocs/sign_in_block/sign_in_bloc.dart';
+import 'package:status_app/screens/auth/views/sign_in_screen.dart';
 import 'components/login_screen_top_image.dart';
 import 'package:status_app/responsive.dart';
 import 'package:status_app/components/background.dart';
@@ -26,18 +29,22 @@ class MobileLoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    final signInBloc =
+        SignInBloc(context.read<AuthenticationBloc>().userRepository);
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        LoginScreenTopImage(),
+      children: [
+        const LoginScreenTopImage(),
         Row(
           children: [
-            Spacer(),
+            const Spacer(),
             Expanded(
-              flex: 8,
-              child: LoginForm(),
-            ),
-            Spacer(),
+                flex: 8,
+                child: BlocProvider<SignInBloc>(
+                  create: (_) => signInBloc,
+                  child: const SignInScreen(),
+                )),
+            const Spacer(),
           ],
         ),
       ],
